2f12ecb74df408f20cc1e7812d39b07420202171
Dolores <doloresperezlaborda@gmail.com>
2024-05-23 20:06:12 -0300

    fix gitignore file

--- backend/dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("dotenv/config");
-const server_1 = require("./features/presentation/server");
-const server = new server_1.Server();
-server.listen();
